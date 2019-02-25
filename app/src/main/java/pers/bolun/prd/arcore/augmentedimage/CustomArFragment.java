82bb037f361d47239970b4f9daf40537ef4019b8
Unknown <zhangbolun2356@gmail.com>
2019-02-25 09:43:29 +0100

    Add CustomArFragment

---
+++ app/src/main/java/pers/bolun/prd/arcore/augmentedimage/CustomArFragment.java
@@ -0,0 +1,11 @@
+package pers.bolun.prd.arcore.augmentedimage;
+
+import android.util.Log;
+
+import com.google.ar.core.Config;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class CustomArFragment {
+    
+}
