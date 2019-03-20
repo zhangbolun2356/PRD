9f50d742fc7acdb4b95208181c5a0377a42393da
Unknown <zhangbolun2356@gmail.com>
2019-03-20 16:47:24 +0100

    Add comment for CustomArFragment
    Explain the Fragment config

--- app/src/main/java/pers/bolun/prd/arcore/augmentedimage/CustomArFragment.java
+++ app/src/main/java/pers/bolun/prd/arcore/augmentedimage/CustomArFragment.java
@@ -5,8 +5,17 @@ import com.google.ar.core.Config;
 import com.google.ar.core.Session;
 import com.google.ar.sceneform.ux.ArFragment;
 
+/**
+ * Extend the ArFragment to customize the ARCore session configuration to include Augmented Images.
+ */
 public class CustomArFragment extends ArFragment {
 
+    //First of all, we are setting the plane discovery instruction to null.
+    //By doing this, we turn off that hand icon which appears just after the fragment is initialized which instructs the user to move his phone around.
+    //We don’t need it any more as we are not detecting random planes but a specific image.
+    //Next, we set the update mode for the session to LATEST_CAMERA_IMAGE.
+    //This ensures that update listener is called whenever the camera frame updates.
+    //It configures the behavior of update method.
     @Override
     protected Config getSessionConfiguration(Session session) {
         getPlaneDiscoveryController().setInstructionView(null);
